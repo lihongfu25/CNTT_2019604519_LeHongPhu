19e4b8f9ec9ad4d2b31bd59dd4182efd659fccec
lihongfu25 <phulh@vmms.com.vn>
2023-04-06 17:40:54 +0700

    fix: change svg

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactSVG } from "react-svg";
 import { useSelector } from "react-redux";
 import { Link, Outlet } from "react-router-dom";
 import "../styles/layout/main.scss";
@@ -36,77 +37,126 @@ const MainLayout = ({ children }) => {
                                 onClick={() => changeCurrentTab("dashboard")}
                             >
                                 {isCollapsed ? (
-                                    <img
+                                    <ReactSVG
                                         src='images/logo.svg'
-                                        alt='logo'
                                         className='main-nav__header__logo__collapse mx-auto appear'
                                     />
                                 ) : (
-                                    <img
+                                    <ReactSVG
                                         src='images/Group 1.svg'
-                                        alt='logo'
                                         className='main-nav__header__logo__expand mx-auto appear'
                                     />
                                 )}
                             </Link>
                         </div>
                     </div>
-                    <div className='px-4 flex-grow-1 overflow-hidden'>
+                    <div className='flex-grow-1 overflow-hidden'>
                         <Link
                             to='/'
-                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 rounded-pill w-100 p-3 mb-2 transition-ease ${
+                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 w-100 py-3 px-4 transition-ease ${
                                 currentTab === "dashboard" && "active"
                             }`}
                             onClick={() => changeCurrentTab("dashboard")}
                         >
-                            <div className='d-flex'>
-                                {currentTab === "dashboard" ? (
-                                    <img
-                                        src='images/icon/dashboard-active.svg'
-                                        alt='logo'
-                                        className=''
-                                    />
-                                ) : (
-                                    <img
-                                        src='images/icon/dashboard.svg'
-                                        alt='logo'
-                                        className=''
-                                    />
-                                )}
+                            <div className='d-flex nav__btn__icon'>
+                                <svg
+                                    className='nav__btn__icon__svg'
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    xmlnsXlink='http://www.w3.org/1999/xlink'
+                                    width='17'
+                                    height='17'
+                                    viewBox='0 0 17 17'
+                                >
+                                    <defs>
+                                        <clipPath id='clip-path'>
+                                            <rect
+                                                id='Rectangle_3'
+                                                data-name='Rectangle 3'
+                                                width='17'
+                                                height='17'
+                                                stroke='#76797e'
+                                                strokeWidth='1'
+                                            />
+                                        </clipPath>
+                                    </defs>
+                                    <g
+                                        id='Mask_Group_2'
+                                        data-name='Mask Group 2'
+                                        clipPath='url(#clip-path)'
+                                    >
+                                        <g
+                                            id='dashboard'
+                                            transform='translate(0 0)'
+                                        >
+                                            <path
+                                                id='Path_3'
+                                                data-name='Path 3'
+                                                d='M16.864,8.364H10.682a.773.773,0,0,1-.773-.773V1.409a.773.773,0,0,1,.773-.773h6.182a.773.773,0,0,1,.773.773V7.591A.773.773,0,0,1,16.864,8.364ZM11.455,6.818h4.636V2.182H11.455Zm2.318,10.818a3.864,3.864,0,1,1,3.864-3.864A3.864,3.864,0,0,1,13.773,17.636Zm0-6.182a2.318,2.318,0,1,0,2.318,2.318A2.318,2.318,0,0,0,13.773,11.455ZM7.591,9.909H1.409a.773.773,0,0,0-.773.773v6.182a.773.773,0,0,0,.773.773H7.591a.773.773,0,0,0,.773-.773V10.682A.773.773,0,0,0,7.591,9.909Zm-.773,6.182H2.182V11.455H6.818Zm.773-7.727H1.409a.773.773,0,0,1-.773-.773V1.409A.773.773,0,0,1,1.409.636H7.591a.773.773,0,0,1,.773.773V7.591A.773.773,0,0,1,7.591,8.364ZM2.182,6.818H6.818V2.182H2.182Z'
+                                                transform='translate(-0.636 -0.636)'
+                                            />
+                                        </g>
+                                    </g>
+                                </svg>
                             </div>
-                            <div className='nav__btn__text appear ms-4'>
-                                Trang chủ
+                            <div className='nav__btn__text appear ms-2'>
+                                Dashboard
                             </div>
                         </Link>
                         <Link
                             to='/project'
-                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 rounded-pill w-100 p-3 my-2 transition-ease ${
+                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 w-100 py-3 px-4 transition-ease ${
                                 currentTab === "project" && "active"
                             }`}
                             onClick={() => changeCurrentTab("project")}
                         >
-                            <div className='d-flex'>
-                                {currentTab === "project" ? (
-                                    <img
-                                        src='images/icon/project-active.svg'
-                                        alt='logo'
-                                        className=''
-                                    />
-                                ) : (
-                                    <img
-                                        src='images/icon/project.svg'
-                                        alt='logo'
-                                        className=''
-                                    />
-                                )}
+                            <div className='d-flex nav__btn__icon'>
+                                <svg
+                                    className=' nav__btn__icon__svg'
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    xmlnsXlink='http://www.w3.org/1999/xlink'
+                                    width='17'
+                                    height='17'
+                                    viewBox='0 0 17 17'
+                                >
+                                    <defs>
+                                        <clipPath id='clip-path'>
+                                            <rect
+                                                id='Rectangle_44'
+                                                data-name='Rectangle 44'
+                                                width='17'
+                                                height='17'
+                                                transform='translate(24 131)'
+                                                stroke='#76797e'
+                                                strokeWidth='1'
+                                            />
+                                        </clipPath>
+                                    </defs>
+                                    <g
+                                        id='Mask_Group_19'
+                                        data-name='Mask Group 19'
+                                        transform='translate(-24 -131)'
+                                        clipPath='url(#clip-path)'
+                                    >
+                                        <g
+                                            id='calendar'
+                                            transform='translate(24 131)'
+                                        >
+                                            <path
+                                                id='Path_584'
+                                                data-name='Path 584'
+                                                d='M13.718,1.309h-.634V.668a.664.664,0,1,0-1.328,0v.642H5.244V.668a.664.664,0,1,0-1.328,0v.642H3.282A3.286,3.286,0,0,0,0,4.591v9.123A3.286,3.286,0,0,0,3.282,17H13.718A3.286,3.286,0,0,0,17,13.714V4.591a3.286,3.286,0,0,0-3.282-3.282ZM3.282,2.637h.634V3.931a.664.664,0,0,0,1.328,0V2.637h6.512V3.931a.664.664,0,0,0,1.328,0V2.637h.634a1.957,1.957,0,0,1,1.955,1.955v.634H1.328V4.591A1.957,1.957,0,0,1,3.282,2.637ZM13.718,15.669H3.282a1.957,1.957,0,0,1-1.955-1.955V6.553H15.672v7.161A1.957,1.957,0,0,1,13.718,15.669ZM5.9,9.164a.664.664,0,0,1-.664.664H3.927a.664.664,0,0,1,0-1.328H5.233A.664.664,0,0,1,5.9,9.164Zm7.84,0a.664.664,0,0,1-.664.664H11.767a.664.664,0,1,1,0-1.328h1.305A.664.664,0,0,1,13.736,9.164Zm-3.924,0a.664.664,0,0,1-.664.664H7.844a.664.664,0,1,1,0-1.328H9.149A.664.664,0,0,1,9.813,9.164ZM5.9,13.08a.664.664,0,0,1-.664.664H3.927a.664.664,0,0,1,0-1.328H5.233A.664.664,0,0,1,5.9,13.08Zm7.84,0a.664.664,0,0,1-.664.664H11.767a.664.664,0,1,1,0-1.328h1.305A.664.664,0,0,1,13.736,13.08Zm-3.924,0a.664.664,0,0,1-.664.664H7.844a.664.664,0,1,1,0-1.328H9.149A.664.664,0,0,1,9.813,13.08Z'
+                                            />
+                                        </g>
+                                    </g>
+                                </svg>
                             </div>
                             <div className='nav__btn__text appear ms-4'>
-                                Dự án
+                                Calendar
                             </div>
                         </Link>
                         <Link
                             to='/task'
-                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 rounded-pill w-100 p-3 my-2 transition-ease ${
+                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 w-100 py-3 px-4 transition-ease ${
                                 currentTab === "task" && "active"
                             }`}
                             onClick={() => changeCurrentTab("task")}
@@ -133,7 +183,7 @@ const MainLayout = ({ children }) => {
                         {user.roleId === ADMIN_ROLE && (
                             <Link
                                 to='/user'
-                                className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 rounded-pill w-100 p-3 my-2 transition-ease ${
+                                className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 w-100 py-3 px-4 transition-ease ${
                                     currentTab === "users" && "active"
                                 }`}
                                 onClick={() => changeCurrentTab("users")}
@@ -160,7 +210,7 @@ const MainLayout = ({ children }) => {
                         )}
                         <Link
                             to='/report'
-                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 rounded-pill w-100 p-3 my-2 transition-ease ${
+                            className={`nav__btn d-flex align-items-center text-decoration-none overflow-hidden border-0 w-100 py-3 px-4 transition-ease ${
                                 currentTab === "report" && "active"
                             }`}
                             onClick={() => changeCurrentTab("report")}
@@ -258,7 +308,7 @@ const MainLayout = ({ children }) => {
                                 </ul>
                             </div>
                         </div>
-                        <div className='p-3'>
+                        <div className='py-3 px-4'>
                             <Outlet />
                         </div>
                     </div>
